e7d6ea514222b9707a2d996fae75909eab733263
Navid <navidcaesar@gmail.com>
2021-11-16 17:17:13 +0800

    Add validation middleware for ad apis with using the express validator

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const ads = require("../controllers/ads");
+const validateAds = require("../middleware/adsValidator");
 
-router.post("/ads", ads.createAd);
-router.get("/ads/:id", ads.getAd);
+router.post("/ads", validateAds.validateCreateAd, ads.createAd);
+router.get("/ads/:id", validateAds.validateId, ads.getAd);
 router.get("/ads", ads.getAds);
-router.delete("/ads/:id", ads.deleteAd);
-router.put("/ads/:id", ads.updateAd);
+router.delete("/ads/:id", validateAds.validateId, ads.deleteAd);
+router.put("/ads/:id", validateAds.validateUpdateAd, ads.updateAd);
 module.exports = router;
